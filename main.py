403db9446d1b2e8a6ae04e5b8be0e885466f5b94
carnasci <cen.master24@gmail.com>
2024-12-26 18:10:02 -0600

    Day 18 - Hirst Dot painting color pallet extracted

---
+++ main.py
@@ -0,0 +1,15 @@
+# import colorgram
+#
+# rgb_colors = []
+# colors = colorgram.extract('image.jpg', 20)
+#
+# for color in colors:
+#     r = color.rgb.r
+#     g = color.rgb.g
+#     b = color.rgb.b
+#     new_color = (r, g, b)
+#     rgb_colors.append(new_color)
+#
+# print(rgb_colors)
+
+color_list = [(208, 157, 105), (228, 241, 238), (58, 98, 132), (156, 83, 53), (139, 159, 189), (231, 202, 104), (158, 167, 41), (52, 174, 121), (122, 189, 173), (201, 136, 154), (65, 39, 32), (76, 113, 88), (126, 80, 88), (133, 30, 48), (28, 48, 67), (195, 93, 74), (181, 93, 110)]
